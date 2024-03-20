14def3b8759f58f647d209f97654922c74332a48
Wiktor Jankowski Ostrowski <122821918+WiktorPWR@users.noreply.github.com>
2024-03-20 17:39:01 +0100

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "dynamic_array.cpp"
+#include "TwoWayList.cpp"
+
+int main()
+{
+    TwoWayList <int> clasa; 
+    for (int i = 0; i <= 10; i++)
+    {
+        //clasa.push_back(i);
+        //clasa.pop_back();
+        clasa.push_front(i);
+        //clasa.pop_front();
+    }
+    clasa.pop_back();
+    //clasa.print_double_list();
+    //clasa.print_array();
+    std::cout << "hello" << std::endl;
+    return 0;
+}
